fd985a4e9050aaa38f83f3dac4431d00871ca7c2
omardwwm <boudraa.omar@gmail.com>
2022-03-28 17:54:47 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+const PORT = 8000 || process.env.PORT;
+
+io.on("connection", socket => {
+    socket.emit("your id", socket.id);
+    socket.on("send message", body => {
+        // console.log(body);
+        io.emit("message", body)
+    })
+})
+
+server.listen(PORT, () => console.log(`server node-express-socket is running on port ${PORT}`));
